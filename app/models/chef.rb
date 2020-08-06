0eec0d3835cf0e9e214dd6a94222c8acd3dbdb30
Linju Jose <linjujose@gmail.com>
2020-08-06 13:47:55 -0700

    add chef destroy action

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -6,7 +6,7 @@ class Chef < ApplicationRecord
                         format: {with: VALID_EMAIL_REGEX},
                         uniqueness: {case_sensitive: false}
 
-    has_many :recipes
+    has_many :recipes, dependent: :destroy
     has_secure_password # enforces password won't be nil at signup
     validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 end
--- test/models/chef_test.rb
+++ test/models/chef_test.rb
@@ -76,4 +76,13 @@ class ChefTest < ActiveSupport::TestCase
         @chef.password = @chef.password_confirmation = "3" * 5
         assert_not @chef.valid?
     end
+
+    test "associated recipes should be destroyed" do
+        @chef.save
+        @chef.recipes.create!(name: "testing destroy", description: "testing destroy feature")
+        assert_difference 'Recipe.count', -1 do
+            @chef.destroy
+        end
+    end
+
 end
