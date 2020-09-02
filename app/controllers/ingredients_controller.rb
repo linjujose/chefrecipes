4538680a8597ca3a356c8790f274988cc8168755
Linju Jose <linjujose@gmail.com>
2020-09-01 23:59:24 -0700

    add ingredient

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -3,16 +3,25 @@ class IngredientsController < ApplicationController
     before_action :set_ingredient, only: [:show, :edit, :update]
 
     def new
-        Ingredient.new
+        @ingredient = Ingredient.new
     end
 
     def create
+        @ingredient = Ingredient.new(ingredient_params)
+        if @ingredient.save
+            flash[:success] = "Ingredient was successfully created"
+            redirect_to ingredient_path(@ingredient)
+        else
+            render 'new'
+        end
     end
 
     def edit
     end
 
     def update
+        if @ingredient.update(ingredient_params)
+            flash[:success] = "Ingredient name was updated successfully"
     end
 
     def show
@@ -25,9 +34,11 @@ class IngredientsController < ApplicationController
 
 private
 
-
-
     def set_ingredient
         @ingredient = Ingredient.find(params[:id])
+    end
+
+    def ingredient_params
+        params.require(:ingredient).permit(:name)
     end
 end
