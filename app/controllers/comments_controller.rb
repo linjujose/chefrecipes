6caa71db4ddb13f6213207ec4228ca77e986490a
Linju Jose <linjujose@gmail.com>
2020-09-09 00:20:00 -0700

    Complete comments feature

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,23 @@
 class CommentsController < ApplicationController
+    before_action  :require_user #logged_in? check in application controller
+
     def create
+        #debugger
         @recipe = Recipe.find(params[:recipe_id])
+        @comment = @recipe.comments.build(comment_params)
+        @comment.chef = current_chef
+        if @comment.save
+            flash[:success] = "Comment was created successfully"
+            redirect_to recipe_path(@recipe)
+        else
+            flash[:danger] = "Comment was not created"
+            redirect_to :back
+        end
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:description)
     end
 end
